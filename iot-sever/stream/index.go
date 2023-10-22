5729e542a36be6e65a9a20970153dcfa2d81f5cd
ashenye <luren145@gmail.com>
2023-10-22 21:47:17 +0800

    chore

--- iot-sever/stream/index.go
+++ iot-sever/stream/index.go
@@ -1,1 +1,30 @@
 package stream
+
+import (
+	"github.com/google/uuid"
+	ext "github.com/reugn/go-streams/extension"
+	"time"
+)
+
+var (
+	source = make(chan any, 1024)
+)
+
+type KeepAlive struct {
+	Uuid      uuid.UUID
+	Timestamp time.Time
+	Weight    float64
+	Health    float64
+	Geo       struct {
+		Latitude  float64
+		Longitude float64
+	}
+}
+
+func init() {
+	ext.NewChanSource(source)
+}
+
+func StreamInput() chan<- any {
+	return source
+}
