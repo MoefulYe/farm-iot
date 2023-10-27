3b44833316568330a9841607de55836d7e35dbe3
ashenye <luren145@gmail.com>
2023-10-27 17:54:03 +0800

    x

--- iot-sever/db/postgres.go
+++ iot-sever/db/postgres.go
@@ -1,6 +1,7 @@
 package db
 
 import (
+	"context"
 	"github.com/MoefulYe/farm-iot/database/postgres/ent"
 	"github.com/MoefulYe/farm-iot/iot-server/config"
 	"log"
@@ -12,6 +13,10 @@ var (
 
 func init() {
 	client, err := ent.Open("postgres", config.Conf.Postgres)
+	if err != nil {
+		log.Fatalf(err.Error())
+	}
+	err = client.Schema.Create(context.Background())
 	if err != nil {
 		log.Fatalf(err.Error())
 	}
