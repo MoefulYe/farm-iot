0335be8d853f3576958c7c49bf901220a0f889b0
ashenye <luren145@gmail.com>
2023-11-19 20:31:46 +0800

    init

--- web/src/views/cow-info.vue
+++ web/src/views/cow-info.vue
@@ -1,37 +1,38 @@
 <template>
-    <NDataTable :columns="columns" :data="cows" :pagination="pagination"/>
+    <NDataTable :remote="true" :columns="columns" :data="cows" :pagination="pagination" />
 </template>
 
 <script setup lang="ts">
-import { DataTableColumns, NDataTable } from 'naive-ui/lib';
-import { CowInfo } from '../api/cow';
-import { onMounted, reactive, ref } from 'vue'
-import { GetCowInfo } from '../api/cow';
+import { DataTableColumns, NDataTable } from 'naive-ui/lib'
+import { CowInfo } from '../api/cow'
+import { onMounted, ref } from 'vue'
+import { GetCowInfo } from '../api/cow'
 
 const cows = ref<CowInfo[]>([])
-const pagination = reactive({
+const pagination = ref({
     page: 1,
-    pageSize: 20,
+    pageSize: 10,
     pageSizes: [2, 5, 10, 20, 40],
     itemCount: 0,
     showSizePicker: true,
-    onchange(page: number) {
-        pagination.page = page
+    onUpdatePage(page: number) {
+        pagination.value.page = page
         fetch()
     },
     onUpdatePageSize(pageSize: number) {
-        pagination.pageSize = pageSize
+        pagination.value.pageSize = pageSize
+        pagination.value.page = 1
         fetch()
-    },
+    }
 })
 
 const fetch = async () => {
     const { data, cnt } = await GetCowInfo({
-        page: pagination.page,
-        size: pagination.pageSize,
+        page: pagination.value.page,
+        size: pagination.value.pageSize
     })
     cows.value = data
-    pagination.itemCount = cnt
+    pagination.value.itemCount = cnt
 }
 
 onMounted(async () => {
@@ -39,24 +40,29 @@ onMounted(async () => {
 })
 </script>
 
-
 <script lang="ts">
 const columns: DataTableColumns<CowInfo> = [
     { title: '名字', key: 'id' },
     {
-        title: '出生时间', key: 'born_at', render(row) {
+        title: '出生时间',
+        key: 'born_at',
+        render(row) {
             return row.born_at.toISOString()
-        },
+        }
     },
     {
-        title: '死亡时间', key: 'dead_at', render(row) {
-            return row.dead_at?.toISOString() ?? ""
-        },
+        title: '死亡时间',
+        key: 'dead_at',
+        render(row) {
+            return row.dead_at?.toISOString() ?? ''
+        }
     },
     {
-        title: '死因', key: 'reason', render(row) {
-            return row.reason ?? ""
-        },
+        title: '死因',
+        key: 'reason',
+        render(row) {
+            return row.reason ?? ''
+        }
     }
 ]
 </script>
